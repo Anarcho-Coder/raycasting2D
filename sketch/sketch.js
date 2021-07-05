da5b259bd0b753f510e6eecfb1ea986309571b85
Russell Thiessen <65782891+Anarcho-Coder@users.noreply.github.com>
2021-07-05 02:15:52 -0500

    Follow mouse and auto-walk
    Added functionality to get the particle to follow the mouse and walk
    on it's own around the screen. Clicking the mouse will swap between
    these two modes.

--- sketch/sketch.js
+++ sketch/sketch.js
@@ -4,17 +4,22 @@ let particle;
 let xoff = 0;
 let yoff = 100;
 
-function setup() {
-	createCanvas(windowHeight, windowWidth);
+let info = "Click mouse to swap between follow mode and auto-walk mode";
+let text_alignment = 0;
 
-	wall.push(new Boundary(0, 0, 0, windowHeight));
-	wall.push(new Boundary(0, 0, windowWidth, 0));
-	wall.push(new Boundary(0, windowHeight, windowWidth, windowHeight));
-	wall.push(new Boundary(windowWidth, 0, windowWidth, windowHeight));
+let clicked = false;
+
+function setup() {
+	createCanvas(windowHeight, windowHeight);
+
+	wall.push(new Boundary(0, 0, 0, height));
+	wall.push(new Boundary(0, 0, width, 0));
+	wall.push(new Boundary(0, height, width, height));
+	wall.push(new Boundary(width, 0, width, height));
 
 	let wallcount = 5;
 	for (let i = 0; i < wallcount; i++) {
-		wall.push(new Boundary(Math.random() * windowHeight, Math.random() * windowWidth, Math.random() * windowHeight, Math.random() * windowWidth));
+		wall.push(new Boundary(Math.random() * width, Math.random() * width, Math.random() * height, Math.random() * width));
 	}
 
 	particle = new Particle();
@@ -23,15 +28,26 @@ function setup() {
 	for (let w of wall) {
 		w.show();
 	}
+
+	text_alignment = width/2 - (28 * 6) /*half the characters times an estimated font width ;D */;
+}
+
+function mouseClicked() {
+	clicked = !clicked;
 }
 
 function draw() {
 	background(0);
-	particle.update(noise(xoff) * width, noise(yoff) * height);
+	fill(255,255,255);
+	text(info, text_alignment, 30);
+	if (!clicked) {
+		particle.update(noise(xoff) * width-1, noise(yoff) * height-1);
 
-	xoff += 0.01;
-	yoff += 0.01;
-
+		xoff += 0.01;
+		yoff += 0.01;
+	} else {
+		particle.update(mouseX, mouseY);
+	}
 
 	for (let w of wall) {
 		w.show();
